eb87d690f8e22249fb853d7fb3f2c2e35717c4f4
zxncvul <adrian.d.reich@gmail.com>
2025-07-29 21:13:35 +0200

    Update numaKeypad.js

--- numaKeypad.js
+++ numaKeypad.js
@@ -26,8 +26,7 @@ export function createNumericKeypad(containerId = 'bottom-controls') {
     ['7', '8', '9'],
     ['4', '5', '6'],
     ['1', '2', '3'],
-    ['0', ',', '←'],
-    ['C', '%', ':'] // Añadidos % y :
+    ['0', ',', '←']
   ];
 
   keys.forEach(rowKeys => {
